0f5509041d1c2324ca3dbdcbdd5e099ab84baf39
dbstjdwo1000@naver.com <60142311@mju.ac.kr>
2017-11-15 10:39:54 +0900

    name and student id

--- button1.py
+++ button1.py
@@ -2,8 +2,8 @@ from tkinter import *
 
 window =Tk()
 
-b1 = Button(window, text="One")
-b2 = Button(window, text="Two")
+b1 = Button(window, text="sungjae yoon")
+b2 = Button(window, text="60142311")
 b1.grid(row=0, column=0)
 b2.grid(row=1, column=1)
 
