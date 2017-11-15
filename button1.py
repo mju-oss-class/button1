fc13a7f4c28844de7dae61409dd1ec03980de1e3
dbstjdwo1000@naver.com <60142311@mju.ac.kr>
2017-11-15 10:52:45 +0900

    birthday

--- button1.py
+++ button1.py
@@ -4,7 +4,9 @@ window =Tk()
 
 b1 = Button(window, text="sungjae yoon")
 b2 = Button(window, text="60142311")
+b3 = Button(window, text="94/10/11")
 b1.grid(row=0, column=0)
 b2.grid(row=1, column=1)
+b3.grid(row=1, column=0)
 
 window.mainloop()
