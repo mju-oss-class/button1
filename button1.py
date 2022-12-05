54cb05626cb2d56111dfa8915665f6f3a8a03e60
Yeon Ryu <ysryu@mju.ac.kr>
2022-12-05 14:34:44 +0900

    학번 수정

--- button1.py
+++ button1.py
@@ -2,8 +2,8 @@ from tkinter import *
 
 window =Tk()
 
-b1 = Button(window, text="Kildong Hong")
-b2 = Button(window, text="601611222")
+b1 = Button(window, text="Yeon Ryu")
+b2 = Button(window, text="60207777")
 b1.grid(row=0, column=0)
 b2.grid(row=1, column=1)
 
