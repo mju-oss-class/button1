ab677490ee03610a276ec44cdfc7de44a447aee0
Yeonseung Ryu <ysryu@mju.ac.kr>
2023-09-11 14:32:23 +0900

    print name and hakbun

--- button1.py
+++ button1.py
@@ -2,8 +2,8 @@ from tkinter import *
 
 window =Tk()
 
-b1 = Button(window, text="Hong kil-dong")
-b2 = Button(window, text="60201111")
+b1 = Button(window, text="Yeon Ryu")
+b2 = Button(window, text="60221010")
 b1.grid(row=0, column=0)
 b2.grid(row=1, column=1)
 
