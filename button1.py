276b7fc62d0a2fdcf450ab4485a28919b83e77d6
Yeonseung Ryu <ysryu@mju.ac.kr>
2024-09-23 11:30:37 +0900

    modify hakbun

--- button1.py
+++ button1.py
@@ -2,8 +2,8 @@ from tkinter import *
 
 window =Tk()
 
-b1 = Button(window, text="Yeon Ryu")
-b2 = Button(window, text="60221010")
+b1 = Button(window, text="Yeonseung Ryu")
+b2 = Button(window, text="60231010")
 b1.grid(row=0, column=0)
 b2.grid(row=1, column=1)
 
